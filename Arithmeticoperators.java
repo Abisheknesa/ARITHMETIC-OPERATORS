9e8a7a0c342b77601a55cff5e5f70858338f5eb2
Abishek N <151867893+Abisheknesa@users.noreply.github.com>
2024-05-23 20:23:11 +0530

    Add files via upload

---
+++ Arithmeticoperators.java
@@ -0,0 +1,24 @@
+package newas;
+
+public class ArithmaticOperators {
+	
+	static int a=50;
+	static int b=20;
+	static int add=a+b;
+	static int sub=a-b;
+	static int mul=a*b;
+	static float div=a/b;
+	static float remain=a%b;
+	
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Addition:"+add);
+		System.out.println("Subtraction:"+sub);
+		System.out.println("Multiplication:"+mul);
+		System.out.println("Division:"+div);
+		System.out.println("Remainder:"+remain);
+
+	}
+
+}
